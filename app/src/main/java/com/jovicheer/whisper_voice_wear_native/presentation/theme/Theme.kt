bc4ced11ace48f037d7264b9817710a8c7a8b598
joneswang <jonescwang@gmail.com>
2025-07-20 16:38:00 -0400

    first commit

---
+++ app/src/main/java/com/jovicheer/whisper_voice_wear_native/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.jovicheer.whisper_voice_wear_native.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun Whisper_voice_wear_nativeTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "whisper_voice_wear_native"
+include(":app")
+ 
